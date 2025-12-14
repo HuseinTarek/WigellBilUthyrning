30944100dc121c02e4fb4e7e72ef6b84f202ee66
HuseinTarek <duceria@gmail.com>
2025-12-14 12:51:58 +0100

    oh

--- src/main/java/se/wigell/biluthyrning/BilUthyrningApp.java
+++ src/main/java/se/wigell/biluthyrning/BilUthyrningApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WigellkoncernenBilUthyrningApplication {
+public class BilUthyrningApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WigellkoncernenBilUthyrningApplication.class, args);
+		SpringApplication.run(BilUthyrningApp.class, args);
 	}
 
 }
